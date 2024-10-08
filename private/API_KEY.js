0430436f3e70d24409855f0e6f09777c8cb6b911
runninginair <runninginair@gmail.com>
2024-10-08 00:25:59 -0700

    Added cache to loadMoviesByCategory

--- private/API_KEY.js
+++ private/API_KEY.js
@@ -1,9 +1,1 @@
 export const API_KEY = "1f87c6d30b430e953baae63831bd2224";
-
-// API_KEY generated from,
-// Username: Runninginair
-// Password: c#6iupNwcpU8Av@
-
-// Old GIT_HUB_TOKEN = "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";
-// New GitHub token: (exp date: 10-07-2024 + 60 days)
-// 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
